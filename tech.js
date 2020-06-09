21b89f106c3211c997dac57ec165b467aef5afcf
shouya0331 <shouya0331@ezweb.ne.jp>
2020-06-09 17:52:10 +0900

    first commit

---
+++ tech.js
@@ -0,0 +1,34 @@
+window.addEventListener("load", function(){
+
+  const pullDownButton = document.getElementById("lists")
+  const pullDownParents = document.getElementById("pull-down")
+  const pullDownChild = document.querySelectorAll(".pull-down-list")
+  const currentList = document.getElementById("current-list")
+
+  pullDownButton.addEventListener('mouseover', function(){
+    this.setAttribute("style", "background-color:#FFBEDA;");
+  })
+
+  pullDownButton.addEventListener('mouseout', function(){
+    this.removeAttribute("style", "background-color:#FFBEDA;");
+  })
+
+  pullDownButton.addEventListener('click', function() {
+    debugger
+    // モーダルの表示と非表示の設定
+    if (pullDownParents.getAttribute("style") == "display:block;") {
+      pullDownParents.removeAttribute("style", "display:block;");
+    } else {
+      pullDownParents.setAttribute("style", "display:block;");
+    }
+  })
+
+  // コースの値を取得し表示させる
+  pullDownChild.forEach((list) => {
+  list.addEventListener("click", function() {
+      value = this.innerHTML
+      currentList.innerHTML = value
+    })
+  });
+
+})
